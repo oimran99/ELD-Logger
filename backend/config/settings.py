fbf4b6aa063992a6ea19a031d70d40d9e3283c2b
Osama Imran <osamaimran135@gmail.com>
2026-07-26 16:42:08 +0500

    changes to settings..remove stale development secrets

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,8 +26,7 @@ load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    'django-insecure-6op^30&9__apalvmwfndg3!*pe5d$_fkzrg1((vkhdl@^e4-d=',
+    "DJANGO_SECRET_KEY"
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
